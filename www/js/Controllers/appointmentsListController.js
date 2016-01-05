d82f6a8ddc6d26bb1ab91cd00788b1aeec5bf0bd
DevZubair <fly_zubair@hotmail.com>
2016-01-05 14:57:56 -0800

    socket connection

--- www/js/Controllers/appointmentsListController.js
+++ www/js/Controllers/appointmentsListController.js
@@ -1,8 +1,19 @@
-hospitalModule.controller('appointmentsListController', function($scope,$ionicPopup,$state,$ionicSideMenuDelegate) {
-  $scope.toggleLeft = function () {
-    $ionicSideMenuDelegate.toggleLeft();
-  };
+hospitalModule.controller('appointmentsListController', function($scope,$ionicPopup,$state,$ionicSideMenuDelegate,$http,Domain) {
+    $scope.toggleLeft = function () {
+        $ionicSideMenuDelegate.toggleLeft();
+    };
+    $scope.allAppointments = [];
 
+    $http.post(Domain + "getAllAppointments",{MobileID : "HBDSWQWSDEaaFRGTHYJ"}).then (function(response){
 
+        if(response){
+            console.log(response);
+            if (response.data.code == 200){
+                $scope.allAppointments = response.data.content;
+            }
+        }
+    },function(error){
+        console.log(error);
+    });
 });
 
--- www/js/Controllers/doctorsListController.js
+++ www/js/Controllers/doctorsListController.js
@@ -1,25 +1,41 @@
 hospitalModule.controller('doctorsListController', function($scope,$ionicPopup,$state,$ionicSideMenuDelegate,hospitalFactory,$http,Domain) {
-  $scope.toggleLeft = function () {
-    $ionicSideMenuDelegate.toggleLeft();
-  };
+    $scope.toggleLeft = function () {
+        $ionicSideMenuDelegate.toggleLeft();
+    };
 
-  $scope.ID = hospitalFactory.returnClinic();
+    $scope.ID = hospitalFactory.returnClinic();
+    $scope.doctorStatus = '';
 
-  $http.post(Domain + "getClinicDoctors",{ClinicID:$scope.ID}).then (function(response){
+    $http.post(Domain + "getClinicDoctors",{ClinicID:$scope.ID}).then (function(response){
 
-    if(response){
-      console.log(response);
-      $scope.drarray = response.data.content;
-    }
+        if(response){
+            console.log(response);
+            $scope.drarray = response.data.content;
+        }
 
-  },function(error){
-    console.log(error);
-  });
+    },function(error){
+        console.log(error);
+    });
 
     $scope.sendDoctorDetail = function(data){
         hospitalFactory.getterDoctor(data);
         $state.go('doctorDetail');
-    }
+    };
+
+    //Socket connection starts
+
+    var socket = io.connect(Domain);
+    socket.on('connect', function () {
+        console.log('Connected with socket');
+
+
+    });
+    socket.on('doctorStatusMethod', function (data) {
+        console.log('Status method with socket received');
+        $scope.doctorStatus = data;
+        console.log($scope.doctorStatus);
+    });
+
 });
 
 
