e54cd1e80e503a4463ae2afbf7ddf846b24ce307
DevZubair <fly_zubair@hotmail.com>
2016-01-13 17:26:17 +0500

    appointment list modal complete. UI bug remaining

--- www/js/Controllers/appointmentsListController.js
+++ www/js/Controllers/appointmentsListController.js
@@ -2,27 +2,43 @@ hospitalModule.controller('appointmentsListController', function($scope,MobileID
   $scope.toggleLeft = function () {
     $ionicSideMenuDelegate.toggleLeft();
   };
+  var today = new Date();
+  today.setHours(0,0,0,0);
 
   $scope.showSpinner = true;
-    $scope.validation= false;
+  $scope.validation= false;
+  $scope.rate =0;
 
-    $scope.current = true;
+  $scope.rating = 4;
+  $scope.data = {
+    rating : 1,
+    max: 5
+  };
+  $scope.user = {
+    comments : ''
+  };
+  $scope.current = true;
   $scope.MobileID = MobileID;
 
-  $http.post(Domain + "getAllAppointments",{MobileID:$scope.MobileID}).then(function(response){
+  $scope.getAppointmentsFunc = function () {
 
-    if(response.data.code==200){
-      console.log(response);
-      $scope.appointments = response.data.content;
-      $scope.showSpinner = false;
-    }else{
-      console.log(response);
-      $scope.showSpinner = false;
+    $http.post(Domain + "getAllAppointments",{MobileID:$scope.MobileID}).then(function(response){
+
+      if(response.data.code==200){
+        console.log(response);
+        $scope.appointments = response.data.content;
+
+        $scope.showSpinner = false;
+      }else{
+        console.log(response);
+        $scope.showSpinner = false;
         $scope.validation= true;
-    }},function(error){
-    console.log(error);
-    $scope.showSpinner = false;
-  });
+      }},function(error){
+      console.log(error);
+      $scope.showSpinner = false;
+    });
+  };
+  $scope.getAppointmentsFunc();
 
   //Socket connection
   var socket = io.connect(Domain);
@@ -47,8 +63,11 @@ hospitalModule.controller('appointmentsListController', function($scope,MobileID
 
   $scope.appointmentDetail = function (device) {
     if(device.Past == true){
+      $scope.ClinicID = device.ClinicID;
+      $scope.DoctorID = device.DoctorID;
       $scope.showModal(device);
-    }else{
+    }
+    else{
       localStorage.setItem('ClinicID',device.ClinicID);
       localStorage.setItem('DoctorID',device.DoctorID);
       localStorage.setItem('appointNumber',device.AppointmentNumber);
@@ -59,7 +78,7 @@ hospitalModule.controller('appointmentsListController', function($scope,MobileID
   $scope.getAppointments = function (num) {
     $scope.appointments = [];
     $scope.showSpinner = true;
-      $scope.validation=false;
+    $scope.validation=false;
 
     if(num==0){
 
@@ -73,7 +92,7 @@ hospitalModule.controller('appointmentsListController', function($scope,MobileID
         }else{
           console.log(response);
           $scope.showSpinner = false;
-            $scope.validation= true;
+          $scope.validation= true;
         }},function(error){
         console.log(error);
         $scope.showSpinner = false;
@@ -91,7 +110,7 @@ hospitalModule.controller('appointmentsListController', function($scope,MobileID
         }else{
           console.log(response);
           $scope.showSpinner = false;
-            $scope.validation= true;
+          $scope.validation= true;
         }},function(error){
         console.log(error);
         $scope.showSpinner = false;
@@ -109,14 +128,46 @@ hospitalModule.controller('appointmentsListController', function($scope,MobileID
   });
 
   $scope.showModal = function (detail) {
-    console.log(detail)
+    console.log(detail);
     $scope.pastAppointment = detail;
-    $scope.mycomment = $scope.pastAppointment.Reviews.comments;
+    $scope.user.comments = $scope.pastAppointment.Reviews.comments;
+    $scope.data.rating = $scope.pastAppointment.Reviews.stars;
     $scope.modal.show();
   };
   $scope.$on('$destroy', function() {
     $scope.modal.remove();
   });
 
+  $scope.$watch('data.rating', function() {
+   // console.log('New value: '+ $scope.data.rating);
+    $scope.rate = $scope.data.rating;
+  });
+
+  $scope.thanks = function() {
+
+    $http.post(Domain + "addReview", {
+      ClinicID: $scope.ClinicID,
+      DoctorID: $scope.DoctorID,
+      MobileID: $scope.MobileID,
+      comments:$scope.user.comments,
+      rating:$scope.rate
+
+    }).then(function (response) {
+
+      if (response) {
+        console.log(response);
+        if (response.data.code == 200) {
+          $scope.getAppointmentsFunc();
+          $scope.modal.hide();
+        }
+        else {
+          $scope.getAppointmentsFunc();
+          $scope.modal.hide();
+        }
+      }
+    }, function (error) {
+      console.log(error);
+    });
+  };
 });
 
