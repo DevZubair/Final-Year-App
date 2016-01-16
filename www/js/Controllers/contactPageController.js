f7233a92cb482a76d31fb94754291bfc0cb1002f
DevZubair <fly_zubair@hotmail.com>
2016-01-16 15:39:35 +0500

    app testing phase 1

--- www/js/Controllers/contactPageController.js
+++ www/js/Controllers/contactPageController.js
@@ -19,90 +19,78 @@ hospitalModule.controller('contactPageController', function($scope,MobileID,$ion
   $scope.PatientAge = '';
   $scope.PatientGender= '';
 
-    $scope.doctorName = localStorage.getItem('Doctor');
-    var doctor = JSON.parse($scope.doctorName);
+  $scope.doctorName = localStorage.getItem('Doctor');
+  var doctor = JSON.parse($scope.doctorName);
 
-    $scope.clinicName = localStorage.getItem('Clinic');
-    var clinic = JSON.parse($scope.clinicName);
-    $scope.appoint=function(){
-        //This if is used for checking validation
-        if($scope.PatientFirstName == ''|| $scope.PatientLastName == ''|| $scope.PatientAge == ''|| $scope.PatientGender == ''){
-            $scope.emptyField= true;
-        }
-        else{
+  $scope.clinicName = localStorage.getItem('Clinic');
+  var clinic = JSON.parse($scope.clinicName);
+  $scope.appoint=function() {
+    //This if is used for checking validation
+    if ($scope.PatientFirstName == '' || $scope.PatientLastName == '' || $scope.PatientAge == '' || $scope.PatientGender == '') {
+      $scope.emptyField = true;
+    }
+    else {
 //if validation is true than it will call the API
-            $ionicBackdrop.retain();
-            $http.post(Domain + "addAppointment",
-                {
-                    ClinicID:$scope.ClinicID,
-                    MobileID : $scope.MobileID,
-                    DoctorID:$scope.DoctorID,
-                    PatientFirstName : $scope.PatientFirstName,
-                    PatientLastName :  $scope.PatientLastName,
-                    PatientAge :  $scope.PatientAge,
-                    Gender : $scope.PatientGender,
-                    DoctorName : doctor.DoctorFirstName + ' ' + doctor.DoctorLastName,
-                    ClinicName : clinic.Name
+      $ionicBackdrop.retain();
+      $http.post(Domain + "addAppointment",
+        {
+          ClinicID: $scope.ClinicID,
+          MobileID: $scope.MobileID,
+          DoctorID: $scope.DoctorID,
+          PatientFirstName: $scope.PatientFirstName,
+          PatientLastName: $scope.PatientLastName,
+          PatientAge: $scope.PatientAge,
+          Gender: $scope.PatientGender,
+          DoctorName: doctor.DoctorFirstName + ' ' + doctor.DoctorLastName,
+          ClinicName: clinic.Name
 
-                }).then (function(response){
+        }).then(function (response) {
 
-                if(response){
-                    console.log(response);
-                    if (response.data.code == 200){
-                        localStorage.setItem('appointNumber',response.data.AppointmentNumber);
-                        localStorage.setItem('appointID',response.data.content._id);
-                        $ionicBackdrop.release();
-                        $state.go('appointmentDetail')
-                    }
-                    else{
-                        $scope.showAlert();
-                        $ionicBackdrop.release();
-                    }
-                }
+          if (response) {
+            console.log(response);
+            if (response.data.code == 200) {
 
-      if(response){
-        console.log(response);
-        if (response.data.code == 200){
+              socket.emit('appointmentAdded', {
+                ClinicID: $scope.ClinicID,
+                MobileID: $scope.MobileID,
+                DoctorID: $scope.DoctorID,
+                PatientFirstName: $scope.PatientFirstName,
+                PatientLastName: $scope.PatientLastName,
+                PatientAge: $scope.PatientAge,
+                Gender: $scope.PatientGender,
+                DoctorName: doctor.DoctorFirstName + ' ' + doctor.DoctorLastName,
+                ClinicName: clinic.Name,
+                Maker: 'Mobile',
+                AppointmentNumber : response.data.AppointmentNumber
+              });
 
-          socket.emit('appointmentAdded', {
-            ClinicID:$scope.ClinicID,
-            MobileID : $scope.MobileID,
-            DoctorID:$scope.DoctorID,
-            PatientFirstName : $scope.PatientFirstName,
-            PatientLastName :  $scope.PatientLastName,
-            PatientAge :  $scope.PatientAge,
-            Gender : $scope.PatientGender,
-            DoctorName : doctor.DoctorFirstName + ' ' + doctor.DoctorLastName,
-            ClinicName : clinic.Name,
-            Maker : 'Mobile'
-          });
+              localStorage.setItem('appointNumber', response.data.AppointmentNumber);
+              localStorage.setItem('appointID', response.data.content._id);
+              $ionicBackdrop.release();
+              $state.go('appointmentDetail')
+            }
+            else {
+              $scope.showAlert();
+              $ionicBackdrop.release();
+            }
+          }
 
-          localStorage.setItem('appointNumber',response.data.AppointmentNumber);
-          localStorage.setItem('appointID',response.data.content._id);
+        }, function (error) {
           $ionicBackdrop.release();
-          $state.go('appointmentDetail')
-        }
-        else{
-          $scope.showAlert();
-          $ionicBackdrop.release();
-        }
-      }
+          console.log(error);
+        });
+    }
+    ;
+    $scope.showAlert = function () {
+      var alertPopup = $ionicPopup.alert({
+        title: 'Appointment Found',
+        template: 'You already had an appointment today with this doctor. Thanks'
+      });
 
-    },function(error){
-      $ionicBackdrop.release();
-      console.log(error);
-    });
-  };
-  $scope.showAlert = function() {
-    var alertPopup = $ionicPopup.alert({
-      title: 'Appointment Found',
-      template: 'You already had an appointment today with this doctor. Thanks'
-    });
-
-    alertPopup.then(function(res) {
-      console.log('Appointment found');
-    });
-  };
-
+      alertPopup.then(function (res) {
+        console.log('Appointment found');
+      });
+    };
+  }
 });
 
--- www/js/Controllers/doctorDetailController.js
+++ www/js/Controllers/doctorDetailController.js
@@ -1,53 +1,80 @@
 hospitalModule.controller('doctorDetailController', function($scope,$ionicPopup,$state,$ionicSideMenuDelegate,$http,hospitalFactory,$ionicBackdrop,Domain) {
 
-    $scope.ClinicID = localStorage.getItem('ClinicID');
-    $scope.DoctorID = localStorage.getItem('DoctorID');
-    $scope.appointmentCheckbox=false;
-    $scope.appointmentToggle=false;
-    var doctor = localStorage.getItem('Doctor');
-    $scope.doctorName = JSON.parse(doctor);
+  $scope.ClinicID = localStorage.getItem('ClinicID');
+  $scope.DoctorID = localStorage.getItem('DoctorID');
+  $scope.appointmentCheckbox=false;
+  $scope.appointmentToggle=false;
+  var doctor = localStorage.getItem('Doctor');
+  $scope.doctorName = JSON.parse(doctor);
 
-    $scope.toggleLeft = function () {
-        $ionicSideMenuDelegate.toggleLeft();
-    };
-    // A confirm dialog
-    $scope.showConfirm = function() {
-        $scope.appointmentToggle = true;
-        var confirmPopup = $ionicPopup.confirm({
-            title: 'Appoint a number',
-            template: 'Are you sure you want to appoint a number for this doctor?'
-        });
-        confirmPopup.then(function(res) {
-            if(res) {
-                console.log('You are sure');
-                $state.go("contactPage");
-            } else {
-                console.log('You are not sure');
-                $scope.appointmentCheckbox=false;
-                $scope.appointmentToggle = false;
-            }
-        });
-    };
+  var socket = io.connect(Domain);
+  socket.on('connect', function(){
+
+    console.log('Connected to socket');
+
+  });
+
+  socket.on('device_active', function (data) {
+    console.log('updated');
+    $scope.drdetail.CurrentNumber = data.nowServing;
+    $scope.drdetail.WaitingPersons = data.inWaiting-1;
+    $scope.$apply($scope.drdetail);
+    if($scope.drdetail.CurrentNumber == localStorage.getItem('appointNumber')){
+      $scope.showPopup();
+    }
+  });
+
+  socket.on('appointmentAdded', function (data) {
+    console.log(data);
+    if(data.DoctorID ==  $scope.DoctorID){
+      $scope.drdetail.WaitingPersons =  $scope.drdetail.WaitingPersons + 1;
+      $scope.$apply($scope.drdetail);
+    }
+
+  });
+
+  $scope.toggleLeft = function () {
+    $ionicSideMenuDelegate.toggleLeft();
+  };
+  // A confirm dialog
+  $scope.showConfirm = function() {
+    $scope.appointmentToggle = true;
+    var confirmPopup = $ionicPopup.confirm({
+      title: 'Appoint a number',
+      template: 'Are you sure you want to appoint a number for this doctor?'
+    });
+    confirmPopup.then(function(res) {
+      if(res) {
+        console.log('You are sure');
+        $state.go("contactPage");
+      } else {
+        console.log('You are not sure');
+        $scope.appointmentCheckbox=false;
+        $scope.appointmentToggle = false;
+      }
+    });
+  };
 //First of all we made doRefresh function which we will call when user pulls, see in html
-    $scope.doRefresh = function() {
-        $ionicBackdrop.retain();
-        $http.post(Domain + "getMachineDetail",{ClinicID:$scope.ClinicID, DoctorID:$scope.DoctorID}).then (function(response){
+  $scope.doRefresh = function() {
+    $ionicBackdrop.retain();
+    $http.post(Domain + "getMachineDetail",{ClinicID:$scope.ClinicID, DoctorID:$scope.DoctorID}).then (function(response){
 
-            if(response){
-                console.log(response);
+      if(response){
+        console.log(response);
 
-                $scope.drdetail = response.data.content;
-                $ionicBackdrop.release();}
+        $scope.drdetail = response.data.content;
+        $scope.drdetail.WaitingPersons = $scope.drdetail.WaitingPersons.length;
 
-        },function(error){
-            console.log(error);
-            $ionicBackdrop.release();
-        });
-        //This is the same API we are calling.
-        $scope.$broadcast('scroll.refreshComplete'); //This line stops the refresh loader
-    };
+        $ionicBackdrop.release();}
 
+    },function(error){
+      console.log(error);
+      $ionicBackdrop.release();
+    });
+    //This is the same API we are calling.
+    $scope.$broadcast('scroll.refreshComplete'); //This line stops the refresh loader
+  };
 
-    $scope.doRefresh();  //we are calling this function when page loads here.
+  $scope.doRefresh();  //we are calling this function when page loads here.
 });
 
