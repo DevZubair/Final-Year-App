e71c33e635d8fcbc9a72d636ff1a04850b2d5349
DevZubair <fly_zubair@hotmail.com>
2016-01-19 15:34:58 +0500

    contact page validation complete

--- www/js/Controllers/clinicsListController.js
+++ www/js/Controllers/clinicsListController.js
@@ -55,12 +55,5 @@ hospitalModule.controller('clinicsListController', function($scope,$ionicBackdro
         $state.go('doctorsList');
     };
 
-  var a = "Test123TEST";
-  var b = a.replace(/[^a-z0-9]/gi,'');
-  if(a === b){
-    console.log('Success validation');
-  }else{
-    console.log('Error validation');
-  }
 
 });
--- www/js/Controllers/contactPageController.js
+++ www/js/Controllers/contactPageController.js
@@ -18,6 +18,8 @@ hospitalModule.controller('contactPageController', function($scope,$ionicPlatfor
   $scope.PatientLastName = '';
   $scope.PatientAge = '';
   $scope.PatientGender= '';
+  $scope.errorField= false;
+  $scope.emptyField = false;
 
   $scope.doctorName = localStorage.getItem('Doctor');
   var doctor = JSON.parse($scope.doctorName);
@@ -25,60 +27,80 @@ hospitalModule.controller('contactPageController', function($scope,$ionicPlatfor
   $scope.clinicName = localStorage.getItem('Clinic');
   var clinic = JSON.parse($scope.clinicName);
   $scope.appoint=function() {
+
+    $ionicBackdrop.retain();
+    $scope.errorField= false;
+    $scope.emptyField = false;
+
     //This if is used for checking validation
+    var a = $scope.PatientFirstName + $scope.PatientLastName + $scope.PatientAge + $scope.PatientGender;
+    var b = a.replace(/[^a-z0-9]/gi,'');
+
     if ($scope.PatientFirstName == '' || $scope.PatientLastName == '' || $scope.PatientAge == '' || $scope.PatientGender == '') {
       $scope.emptyField = true;
+      $ionicBackdrop.release();
     }
+
     else {
 //if validation is true than it will call the API
-      $ionicBackdrop.retain();
-      $http.post(Domain + "addAppointment",
-        {
-          ClinicID: $scope.ClinicID,
-          MobileID: $scope.MobileID,
-          DoctorID: $scope.DoctorID,
-          PatientFirstName: $scope.PatientFirstName,
-          PatientLastName: $scope.PatientLastName,
-          PatientAge: $scope.PatientAge,
-          Gender: $scope.PatientGender,
-          DoctorName: doctor.DoctorFirstName + ' ' + doctor.DoctorLastName,
-          ClinicName: clinic.Name
+      if(a === b && $scope.PatientAge >=1 && $scope.PatientAge <=130){
 
-        }).then(function (response) {
+        $http.post(Domain + "addAppointment",
+          {
+            ClinicID: $scope.ClinicID,
+            MobileID: $scope.MobileID,
+            DoctorID: $scope.DoctorID,
+            PatientFirstName: $scope.PatientFirstName,
+            PatientLastName: $scope.PatientLastName,
+            PatientAge: $scope.PatientAge,
+            Gender: $scope.PatientGender,
+            DoctorName: doctor.DoctorFirstName + ' ' + doctor.DoctorLastName,
+            ClinicName: clinic.Name
 
-          if (response) {
-            console.log(response);
-            if (response.data.code == 200) {
+          }).then(function (response) {
 
-              socket.emit('appointmentAdded', {
-                ClinicID: $scope.ClinicID,
-                MobileID: $scope.MobileID,
-                DoctorID: $scope.DoctorID,
-                PatientFirstName: $scope.PatientFirstName,
-                PatientLastName: $scope.PatientLastName,
-                PatientAge: $scope.PatientAge,
-                Gender: $scope.PatientGender,
-                DoctorName: doctor.DoctorFirstName + ' ' + doctor.DoctorLastName,
-                ClinicName: clinic.Name,
-                Maker: 'Mobile',
-                AppointmentNumber : response.data.AppointmentNumber
-              });
+            if (response) {
+              console.log(response);
+              if (response.data.code == 200) {
 
-              localStorage.setItem('appointNumber', response.data.AppointmentNumber);
-              localStorage.setItem('appointID', response.data.content._id);
-              $ionicBackdrop.release();
-              $state.go('appointmentDetail')
+                socket.emit('appointmentAdded', {
+                  ClinicID: $scope.ClinicID,
+                  MobileID: $scope.MobileID,
+                  DoctorID: $scope.DoctorID,
+                  PatientFirstName: $scope.PatientFirstName,
+                  PatientLastName: $scope.PatientLastName,
+                  PatientAge: $scope.PatientAge,
+                  Gender: $scope.PatientGender,
+                  DoctorName: doctor.DoctorFirstName + ' ' + doctor.DoctorLastName,
+                  ClinicName: clinic.Name,
+                  Maker: 'Mobile',
+                  AppointmentNumber : response.data.AppointmentNumber
+                });
+
+                localStorage.setItem('appointNumber', response.data.AppointmentNumber);
+                localStorage.setItem('appointID', response.data.content._id);
+                $ionicBackdrop.release();
+                $state.go('appointmentDetail')
+              }
+              else {
+                $scope.showAlert();
+                $ionicBackdrop.release();
+              }
             }
-            else {
-              $scope.showAlert();
-              $ionicBackdrop.release();
-            }
-          }
 
-        }, function (error) {
-          $ionicBackdrop.release();
-          console.log(error);
-        });
+          }, function (error) {
+            $ionicBackdrop.release();
+            console.log(error);
+          });
+
+      }
+      else{
+        console.log('Error validation');
+        $scope.emptyField = false;
+        $scope.errorField= true;
+        $ionicBackdrop.release();
+      }
+
     }
 
     $scope.showAlert = function () {
@@ -95,91 +117,91 @@ hospitalModule.controller('contactPageController', function($scope,$ionicPlatfor
 
   //All code below is for push notifications
 
- /* $scope.notifications = [];
+  /* $scope.notifications = [];
 
-  // call to register automatically upon device ready
+   // call to register automatically upon device ready
 
-  // Register
-  var register = function () {
-    var config = null;
+   // Register
+   var register = function () {
+   var config = null;
 
-    if (ionic.Platform.isAndroid()) {
-      config = {
-        "senderID": "YOUR_GCM_PROJECT_ID" // REPLACE THIS WITH YOURS FROM GCM CONSOLE - also in the project URL like: https://console.developers.google.com/project/434205989073
-      };
-    }
-    else if (ionic.Platform.isIOS()) {
-      config = {
-        "badge": "true",
-        "sound": "true",
-        "alert": "true"
-      }
-    }
+   if (ionic.Platform.isAndroid()) {
+   config = {
+   "senderID": "YOUR_GCM_PROJECT_ID" // REPLACE THIS WITH YOURS FROM GCM CONSOLE - also in the project URL like: https://console.developers.google.com/project/434205989073
+   };
+   }
+   else if (ionic.Platform.isIOS()) {
+   config = {
+   "badge": "true",
+   "sound": "true",
+   "alert": "true"
+   }
+   }
 
-    $cordovaPush.register(config).then(function (result) {
-      console.log("Register success " + result);
+   $cordovaPush.register(config).then(function (result) {
+   console.log("Register success " + result);
 
-      $scope.registerDisabled=true;
-      // ** NOTE: Android regid result comes back in the pushNotificationReceived, only iOS returned here
-      if (ionic.Platform.isIOS()) {
-        $scope.regId = result;
-        localStorage.setItem('RegID' , result);
-      }
-    }, function (err) {
-      console.log("Register error " + err)
-    });
-  };
+   $scope.registerDisabled=true;
+   // ** NOTE: Android regid result comes back in the pushNotificationReceived, only iOS returned here
+   if (ionic.Platform.isIOS()) {
+   $scope.regId = result;
+   localStorage.setItem('RegID' , result);
+   }
+   }, function (err) {
+   console.log("Register error " + err)
+   });
+   };
 
-  // Notification Received
-  $scope.$on('$cordovaPush:notificationReceived', function (event, notification) {
-    console.log(JSON.stringify([notification]));
-    if (ionic.Platform.isAndroid()) {
-      switch(notification.event) {
-        case 'registered':
-          if (notification.regid.length > 0 ) {
-            alert('registration ID = ' + notification.regid);
-            localStorage.setItem('RegID' , notification.regid);
-          }
-          break;
+   // Notification Received
+   $scope.$on('$cordovaPush:notificationReceived', function (event, notification) {
+   console.log(JSON.stringify([notification]));
+   if (ionic.Platform.isAndroid()) {
+   switch(notification.event) {
+   case 'registered':
+   if (notification.regid.length > 0 ) {
+   alert('registration ID = ' + notification.regid);
+   localStorage.setItem('RegID' , notification.regid);
+   }
+   break;
 
-        case 'message':
-          // this is the actual push notification. its format depends on the data model from the push server
-          alert('message = ' + notification.message + ' msgCount = ' + notification.msgcnt);
-          break;
+   case 'message':
+   // this is the actual push notification. its format depends on the data model from the push server
+   alert('message = ' + notification.message + ' msgCount = ' + notification.msgcnt);
+   break;
 
-        case 'error':
-          alert('GCM error = ' + notification.msg);
-          break;
+   case 'error':
+   alert('GCM error = ' + notification.msg);
+   break;
 
-        default:
-          alert('An unknown GCM event has occurred');
-          break;
-      }
-    }
-    else if (ionic.Platform.isIOS()) {
+   default:
+   alert('An unknown GCM event has occurred');
+   break;
+   }
+   }
+   else if (ionic.Platform.isIOS()) {
 
-    }
-  });
-  $ionicPlatform.ready(function() {
-    if (ionic.Platform.isAndroid() || ionic.Platform.isIOS()) {
-      register();
-    }
+   }
+   });
+   $ionicPlatform.ready(function() {
+   if (ionic.Platform.isAndroid() || ionic.Platform.isIOS()) {
+   register();
+   }
 
-  });
+   });
 
-  function storeDeviceToken(type) {
-    // Create a random userid to store with it
-    var user = { user: 'user' + Math.floor((Math.random() * 10000000) + 1), type: type, token: $scope.regId };
-    console.log("Post token for registered device with data " + JSON.stringify(user));
+   function storeDeviceToken(type) {
+   // Create a random userid to store with it
+   var user = { user: 'user' + Math.floor((Math.random() * 10000000) + 1), type: type, token: $scope.regId };
+   console.log("Post token for registered device with data " + JSON.stringify(user));
 
-    $http.post('http://192.168.1.16:8000/subscribe', JSON.stringify(user))
-      .success(function (data, status) {
-        console.log("Token stored, device is successfully subscribed to receive push notifications.");
-      })
-      .error(function (data, status) {
-        console.log("Error storing device token." + data + " " + status)
-      }
-    );
-  }*/
+   $http.post('http://192.168.1.16:8000/subscribe', JSON.stringify(user))
+   .success(function (data, status) {
+   console.log("Token stored, device is successfully subscribed to receive push notifications.");
+   })
+   .error(function (data, status) {
+   console.log("Error storing device token." + data + " " + status)
+   }
+   );
+   }*/
 });
 
